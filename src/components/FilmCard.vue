bd1436228fe01e925eb7165471f436492fa394f9
Gozzissimo <fantini.nico@gmail.com>
2022-01-20 16:10:16 +0100

    import country-flag component

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -4,12 +4,20 @@
         <h2>{{ originalTitle }}</h2>
         <h3>{{ language }}</h3>
         <h4>{{ rating }}</h4>
+        <country-flag country='it' size='small'/>
     </li>
 </template>
 
 <script>
+import CountryFlag from "vue-country-flag";
+
 export default {
     name: "FilmCard",
+
+    components: {
+        CountryFlag,
+    },
+
     props: {
         movieTitle: {
             type: String,
